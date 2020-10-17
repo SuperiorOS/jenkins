e697253d29a79ab1e12a739123ba41879fb0f873
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2020-10-17 15:26:46 +0530

    jenkins: Eleven time
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I2527e61e2636db25cafb0e2e7d2f59a2950414c6

--- sync.sh
+++ sync.sh
@@ -18,7 +18,7 @@ then
    if [ ! -d "$repo" ];
    then
       echo -e "Initializing The Repo"
-      repo init -u https://github.com/SuperiorOS/manifest.git -b ten
+      repo init -u https://github.com/SuperiorOS/manifest.git -b eleven
       sleep 5
       echo -e "Repo has been Initialized, please sync the source"
    fi
